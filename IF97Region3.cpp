a099850fefcb615c17147cd207936dff96dd68df
mengzesam <mengzesam@126.com>
2018-11-13 17:01:18 +0800

    Add files via upload

---
+++ IF97Region3.cpp
@@ -0,0 +1,120 @@
+#include "IF97Region3.h"
+#include "IF97Region4.h"
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+double IF97Region3::TV2P(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi_delta=ni[0]/delta;
+    for(int i =0;i<39;i++){
+        phi_delta+=ni[i+1]*Ii[i]*pow(delta,Ii[i]-1)*pow(tau,Ji[i]);
+    }
+    return delta*phi_delta*R*(t+T0)/(v*1000.0);
+    //in if97--table31 there is not  "/1000"
+}
+double IF97Region3::TV2H(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi_delta=ni[0]/delta;
+    double phi_tau=0;
+    for(int i =0;i<39;i++){
+        phi_delta+=ni[i+1]*Ii[i]*pow(delta,Ii[i]-1)*pow(tau,Ji[i]);
+        phi_tau+=ni[i+1]*pow(delta,Ii[i])*Ji[i]*pow(tau,Ji[i]-1);
+    }
+	return (tau*phi_tau+delta*phi_delta)*R*(t+T0);
+}
+double IF97Region3::TV2U(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi_tau=0;
+    for(int i =0;i<39;i++){
+        phi_tau+=ni[i+1]*pow(delta,Ii[i])*Ji[i]*pow(tau,Ji[i]-1);
+    }
+    return tau*phi_tau*R*(t+T0);
+}
+double IF97Region3::TV2S(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi=ni[0]*log(delta);
+    double phi_tau=0;
+    for(int i =0;i<39;i++){
+		phi+=ni[i+1]*pow(delta,Ii[i])*pow(tau,Ji[i]);
+        phi_tau+=ni[i+1]*pow(delta,Ii[i])*Ji[i]*pow(tau,Ji[i]-1);
+    }
+	return (tau*phi_tau-phi)*R;
+}
+double IF97Region3::TV2Cp(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi_delta=ni[0]/delta;
+    double phi_delta2=-ni[0]/(delta*delta);
+    double phi_tau2=0;
+    double phi_deltatau=0;
+    for(int i =0;i<39;i++){
+        phi_delta+=ni[i+1]*Ii[i]*pow(delta,Ii[i]-1)*pow(tau,Ji[i]);
+        phi_delta2+=ni[i+1]*Ii[i]*(Ii[i]-1)*pow(delta,Ii[i]-2)*pow(tau,Ji[i]);
+		phi_tau2+=ni[i+1]*pow(delta,Ii[i])*Ji[i]*(Ji[i]-1)*pow(tau,Ji[i]-2);
+		phi_deltatau+=ni[i+1]*Ii[i]*pow(delta,Ii[i]-1)*Ji[i]*pow(tau,Ji[i]-1);
+    }
+	double cp=-tau*tau*phi_tau2;
+	cp+=pow(delta*phi_delta-delta*tau*phi_deltatau,2)
+        /(2*delta*phi_delta+delta*delta*phi_delta2);
+    cp*=R;
+    return cp;
+}
+double IF97Region3::TV2Cv(double t,double v){
+    double delta=1.0/(rhoc*v);
+    double tau=Tc/(t+T0);
+    double phi_tau2=0;
+    for(int i =0;i<39;i++){
+		phi_tau2+=ni[i+1]*pow(delta,Ii[i])*Ji[i]*(Ji[i]-1)*pow(tau,Ji[i]-2);
+    }
+	return -tau*tau*phi_tau2*R;
+}
+double IF97Region3::TV2W(double t,double v){
+    return 0.0;
+}
+void verifyHS(){
+    // double p,t,h,s,pp,tt;
+    // int i=0,max=0;
+    // double dp=(100-0.000611657)/200;
+    // for(p=0.000611657;p<=100;p+=dp){
+    //     double maxt=350;
+    //     if(p<=22.064){
+    //         maxt=IF97Region4::P2T(p);
+    //         maxt=maxt>350?350:maxt;
+    //     }
+    //     double dt=maxt/200;
+    //     for(t=0;t<=maxt;t+=dt){
+    //         h=IF97Region3::PT2H(p,t);
+    //         s=IF97Region3::PT2S(p,t);
+    //         IF97Region3::HS2PT(h,s,pp,tt,i);
+    //         if(i>max) max=i;
+    //         cout<<setprecision(10)<<i<<'\t'<<h<<"\t"<<s<<"\t"<<p<<"\t"<<t<<"\t";
+    //         cout<<setprecision(10)<<pp<<'\t'<<tt<<"\t"<<abs(100*(pp-p)/p)<<"\t";
+    //         if(t<1E-9)
+    //             cout<<setprecision(10)<<abs(tt-t)<<endl;
+    //         else
+    //             cout<<setprecision(10)<<abs(100.0*(tt-t)/t)<<endl;
+    //     }
+    // }
+    // cout<<max<<endl;
+}
+int main(){ 
+    double p,t,h,u,s,v,cp,cv,pp,tt,p2;
+    int i;
+    v=1.0/200;
+    t=650-273.15;
+    p=IF97Region3::TV2P(t,v);
+    h=IF97Region3::TV2H(t,v);
+    u=IF97Region3::TV2U(t,v);
+    s=IF97Region3::TV2S(t,v);
+    cp=IF97Region3::TV2Cp(t,v);
+    cv=IF97Region3::TV2Cv(t,v);    
+    return 0;
+} 
---
+++ IF97Region3.h
@@ -0,0 +1,188 @@
+/*
+    reference book:IAPWS R7-97(2012)
+*/
+class IF97Region3{
+private://构造函数为private，禁止实例化IF97Region3
+    IF97Region3(){};
+public: //static function: T,V to 
+    static double TV2P(double t,double v);
+    static double TV2H(double t,double v);
+    static double TV2S(double t,double v);
+    static double TV2U(double t,double v);
+    static double TV2Cp(double t,double v);
+    static double TV2Cv(double t,double v);
+    static double TV2W(double t,double v);
+public: //static function: P,T to 
+    static double PT2H(double p,double t);
+    static double PT2S(double p,double t);
+    static double PT2V(double p,double t);
+    static double PT2U(double p,double t);
+    static double PT2Cp(double p,double t);
+    static double PT2Cv(double p,double t);
+    static double PT2W(double p,double t);
+public: //static function: P,H to 
+    static double PH2T(double p,double h,int& itera);
+    static double PH2T(double p,double h);
+    static double PH2S(double p,double h);
+    static double PH2V(double p,double h);
+    static double PH2U(double p,double h);
+    static double PH2Cp(double p,double h);
+    static double PH2Cv(double p,double h);
+    static double PH2W(double p,double h);
+public: //static function: P,S to 
+    static double PS2T(double p,double s,int& itera);
+    static double PS2T(double p,double s);
+    static double PS2H(double p,double s);
+    static double PS2V(double p,double s);
+    static double PS2U(double p,double s);
+    static double PS2Cp(double p,double s);
+    static double PS2Cv(double p,double s);
+    static double PS2W(double p,double s);
+public: //static function: P,V to 
+    static double PV2T(double p,double v,int&);
+    static double PV2T(double p,double v);
+    static double PV2H(double p,double v);
+    static double PV2S(double p,double v);
+    static double PV2U(double p,double v);
+    static double PV2Cp(double p,double v);
+    static double PV2Cv(double p,double v);
+    static double PV2W(double p,double v);
+public: //static function: P,U to 
+    static double PU2T(double p,double u,int&);
+    static double PU2T(double p,double u);
+    static double PU2H(double p,double u);
+    static double PU2S(double p,double u);
+    static double PU2V(double p,double u);
+    static double PU2Cp(double p,double u);
+    static double PU2Cv(double p,double u);
+    static double PU2W(double p,double u);
+public: //static function: P,Cp to 
+        //由于过冷水相同压力下不同温度的Cp相差很小，PCp2T迭代出的t值很难还原回原来的t
+        //不建议采用P，Cp to求其他参数
+    static double PCp2T(double p,double cp,int&);
+    static double PCp2T(double p,double cp);
+    static double PCp2H(double p,double cp);
+    static double PCp2S(double p,double cp);
+    static double PCp2V(double p,double cp);
+    static double PCp2U(double p,double cp);
+    static double PCp2Cv(double p,double cp);
+    static double PCp2W(double p,double cp);
+public: //static function: T,H to 
+    static double TH2P(double t,double h,double& p2, int& itera);
+    static double TH2P(double t,double h,double& p2);
+    static double TH2S(double t,double h);
+    static double TH2V(double t,double h);
+    static double TH2U(double t,double h);
+    static double TH2Cp(double t,double h);
+    static double TH2Cv(double t,double h);
+    static double TH2W(double t,double h);
+public: //static function: T,S to 
+    static double TS2P(double t,double s,double& p2,int& itera);
+    static double TS2P(double t,double s,double& p2);
+    static double TS2H(double t,double s);
+    static double TS2V(double t,double s);
+    static double TS2U(double t,double s);
+    static double TS2Cp(double t,double s);
+    static double TS2Cv(double t,double s);
+    static double TS2W(double t,double s);
+
+public: //static function: T,U to 
+    static double TU2P(double t,double u,double& p2,int& itera);
+    static double TU2P(double t,double u,double& p2);
+    static double TU2P(double t,double u);
+    static double TU2H(double t,double u);
+    static double TU2S(double t,double u);
+    static double TU2V(double t,double u);
+    static double TU2Cp(double t,double u);
+    static double TU2Cv(double t,double u);
+    static double TU2W(double t,double u);
+public: //static function: T,Cp to 
+    static double TCp2P(double t,double Cp,int& itera);
+    static double TCp2P(double t,double Cp);
+    static double TCp2H(double t,double Cp);
+    static double TCp2S(double t,double Cp);
+    static double TCp2V(double t,double Cp);
+    static double TCp2U(double t,double Cp);
+    static double TCp2Cv(double t,double Cp);
+    static double TCp2W(double t,double Cp);
+public: //static function: T,Cv to 
+    static double TCv2P(double t,double Cv,double&p2,int& itera);
+    static double TCv2P(double t,double Cv,double&p2);
+    static double TCv2H(double t,double Cv);
+    static double TCv2S(double t,double Cv);
+    static double TCv2V(double t,double Cv);
+    static double TCv2U(double t,double Cv);
+    static double TCv2Cv(double t,double Cv);
+    static double TCv2W(double t,double Cv);
+public: //static function: H,S to 
+    static void HS2PT(double h,double s,double& p,double& t,int& itera);
+    static void HS2PT(double h,double s,double& p,double& t);
+private:
+    const static double ERR0;
+    const static double ERR;
+    const static double ERR2;
+    const static double T0;
+    const static double R;
+    const static double Tc;
+    const static double pc;
+    const static double rhoc;
+    const static double ni[40];
+    const static double Ii[39];
+    const static double Ji[39];
+};
+const double IF97Region3::ERR0=1E-5;
+const double IF97Region3::ERR=1E-7;
+const double IF97Region3::ERR2=1E-10;
+const double IF97Region3::T0=273.15;
+const double IF97Region3::R=0.461526;//page5 (1)
+const double IF97Region3::Tc=647.096;//page5 (2)
+const double IF97Region3::pc=22.064;//page5 (3)
+const double IF97Region3::rhoc=322;//page5 (4)
+const double IF97Region3::ni[40]={ //page30 table30
+			 0.10658070028513E1,
+			-0.15732845290239E2,
+			 0.20944396974307E2,
+			-0.76867707878716E1,
+			 0.26185947787954E1,
+			-0.28080781148620E1,
+			 0.12053369696517E1,
+			-0.84566812812502E-2,
+			-0.12654315477714E1,
+			-0.11524407806681E1,
+			 0.88521043984318E0,
+			-0.64207765181607E0,
+			 0.38493460186671E0,
+			-0.85214708824206E0,
+			 0.48972281541877E1,
+			-0.30502617256965E1,
+			 0.39420536879154E-1,
+			 0.12558408424308E0,
+			-0.27999329698710E0,
+			 0.13899799569460E1,
+			-0.20189915023570E1,
+			-0.82147637173963E-2,
+			-0.47596035734923E0,
+			 0.43984074473500E-1,
+			-0.44476435428739E0,
+			 0.90572070719733E0,
+			 0.70522450087967E0,
+			 0.10770512626332E0,
+			-0.32913623258954E0,
+			-0.50871062041158E0,
+			-0.22175400873096E-1,
+			 0.94260751665092E-1,
+			 0.16436278447961E0,
+			-0.13503372241348E-1,
+			-0.14834345352472E-1,
+			 0.57922953628084E-3,
+			 0.32308904703711E-2,
+			 0.80964802996215E-4,
+			-0.16557679795037E-3,
+			-0.44923899061815E-4
+ };
+const double IF97Region3::Ii[39]={//page30 table30
+    0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,4,4,4,4,5,5,5,6,6,6,7,8,9,9,10,10,11
+ };
+const double IF97Region3::Ji[39]={//page30 table30
+    0,1,2,7,10,12,23,2,6,15,17,0,2,6,7,22,26,0,2,4,16,26,0,2,4,26,1,3,26,0,2,26,2,26,2,26,0,1,26
+ };
